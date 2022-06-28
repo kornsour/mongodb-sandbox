f9ca2f11874926712ad698b7a3092c8d3788a30e
kornsour <ajkaiserauer@gmail.com>
2022-06-28 00:45:09 -0400

    adding devcontainer

---
+++ users/app/main.py
@@ -0,0 +1,24 @@
+import logging
+
+from fastapi import FastAPI
+
+from app.api import users
+
+log = logging.getLogger("uvicorn")
+
+def create_application() -> FastAPI:
+    application = FastAPI()
+    application.include_router(users.router, prefix="/users", tags=["summaries"])
+
+    return application
+
+app = create_application()
+
+@app.on_event("startup")
+async def startup_event():
+    log.info("Starting up...")
+    init_db(app)
+
+@app.on_event("shutdown")
+async def shutdown_event():
+    log.info("Shutting down...")
