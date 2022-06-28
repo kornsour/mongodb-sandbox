ec9ba3a987116d494e5a7f2b53b12ffbbd40860a
kornsour <ajkaiserauer@gmail.com>
2022-06-28 22:56:24 +0000

    main

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
+import datetime
+import pprint
 from pymongo import MongoClient
 
 client = MongoClient('localhost', 27017)
 
 db = client.test_database
-print(db.collection_names())
+print(db)
+
+collection = db.test_collection
+print(collection)
+
+post = {"author": "Mike",
+        "text": "My first blog post!",
+        "tags": ["mongodb", "python", "pymongo"],
+        "date": datetime.datetime.utcnow()}
+
+posts = db.posts
+print(posts)
+
+post_id = posts.insert_one(post).inserted_id
+print(post_id)
